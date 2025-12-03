87d00b7a2b591f0ed382d51cc8def9b8246cdc9e
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2025-12-03 15:37:21 -0600

    v0.6 - Pressure Plate
    Added a pressure plate, the first actor! Activates when stood on, deactivates otherwise.

---
+++ scripts/actors/pressure_plate.gd
@@ -0,0 +1,7 @@
+class_name PressurePlate extends Actor
+
+func _on_body_entered(_body: Node3D) -> void: value = true
+func _on_body_exited(_body: Node3D) -> void:  value =  false
+
+func _process(delta: float) -> void:
+	print(value)
